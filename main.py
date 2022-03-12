8a2756a5fe9cd6b80b0a271d39c8b9f2dfc232f6
praguna <praguna.manvi@gmail.com>
2022-03-12 14:26:31 +0530

    update sidebar

--- main.py
+++ main.py
@@ -28,7 +28,21 @@ def data_set_widget(path):
 def get_values_of_data(images):
     l =  [img.split('_')[:-1] for img in images]
     proper = list(map(list, zip(*l)))
-    return [list(set(p)) for p in proper] 
+    return [list(set(p)) for p in proper], l 
+
+
+def suggested_values(img_split, selected_primary_values):
+     r = set()
+     for v in img_split:
+         for e in selected_primary_values:
+            try: v.index(e)
+            except ValueError: continue
+            r.add(','.join(v))
+     return list(r)
+    
+def _update_slider(keys, values):
+    for k, v in zip(keys, values.split(',')):
+        st.session_state[str(k)] = v
         
 
 def exp_config_display_widegt(path):
@@ -80,7 +94,7 @@ if __name__ == '__main__':
 
     info, images = exp_config_display_widegt(curr_path)
 
-    values = get_values_of_data(images)
+    values, img_split = get_values_of_data(images)
 
     if len(images) > 0:
         ext = images[0][images[0].index('_.') + 1 : ]
@@ -89,21 +103,34 @@ if __name__ == '__main__':
     labels = info[config_info]['names'] if info[files_config_present] else None
     init_val = info[config_info]['init'] if info[files_config_present] and 'init' in info[config_info] else None
     primary_index =  info[config_info]['primary_index'] if info[files_config_present] and 'primary_index' in info[config_info] else None
+    if primary_index and labels:
+        primary_index_pos = [labels.index(p) for p in primary_index]
+
     with st.expander("options"):
         for i, v in enumerate(values):
-            label = labels[i] if labels else f'variable {i+1}'
+            label = labels[i] if labels and len(labels) > i else f'variable {i+1}'
             try:
                 mv = sorted([int(e) for e in v])
                 mv = [str(e) for e in mv]
             except Exception:
                 mv = v
             if len(v) == 1: selected_values.append(st.radio(label, mv))
-            else: selected_values.append(st.select_slider(label, mv , value=init_val[i] if init_val else None))
+            else: selected_values.append(st.select_slider(label, mv , value=init_val[i] if init_val and len(init_val) > i else None, key=str(i)))
     try:
         image = Image.open(os.path.join(curr_path, '_'.join(selected_values + [ext])))
         st.image(image, caption='Data')
     except FileNotFoundError:
-        st.warning('This Experiment was found! Try other values!')
+        st.warning('This Experiment was not found! Try other values!')
+        if primary_index_pos:
+            selected_primary_values = [e for p, e in enumerate(selected_values) if p in primary_index_pos]
+            suggestions = suggested_values(img_split, selected_primary_values)
+            st.write('Suggested :')
+            for i in range(min(len(suggestions), 10)):
+                c1, c2 = st.columns(2)
+                with c1:
+                    st.text(suggestions[i])
+                with c2:
+                    st.button('apply', key=f'apply_{i}', on_click=_update_slider, kwargs={"values" : suggestions[i], "keys" : range(len(labels))})
 
 
     # hide streamlit
